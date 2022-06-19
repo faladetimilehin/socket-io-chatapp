997e5098f1746c49ddc0c7586dbbd92c6cffe9ba
faladetimilehin <faladetimilehin@yahoo.com>
2022-06-19 20:54:46 +0100

    improving the chat system

--- server/index.js
+++ server/index.js
@@ -9,9 +9,6 @@ app.use(express.json ());
 
 const server = http.createServer(app);
 
-server.listen(3001, () =>{
-    console.log("Server is running ")
-})
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -21,11 +18,10 @@ const io = new Server(server, {
 
 io.on("connection", (socket) =>{
     console.log(`User connected: ${socket.id}`)
-
   
     socket.on("join_room", (data) =>{
         socket.join(data)
-        console.log(`User joined room: ${data}`)
+        console.log(`User with ID: ${socket.id} joined room: ${data}`)
     })
 
     socket.on("send_message", (data) =>{
@@ -36,8 +32,12 @@ io.on("connection", (socket) =>{
     //     socket.broadcast.emit("receive_message",data)
     // })
     socket.on("disconnect" , () =>{
-        console.log(`User disconnected`)
+        console.log(`User disconnected`,socket.id)
     })
      
 })
 
+
+server.listen(3001, () =>{
+    console.log("Server is running ")
+})
